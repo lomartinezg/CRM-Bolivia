5cab48b0ed531e06a795c7e7a4f2ae95d483524a
lomartinezg <37275194+lomartinezg@users.noreply.github.com>
2021-07-17 08:29:00 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firestore: {
-    apiKey: "AIzaSyDaoXK5i44ILixF01h9aOm7laiZ6A8uQSc",
+    apiKey: "XXXXXXXXXXXXXXXXXXXXX",
     authDomain: "juntos-trinidad.firebaseapp.com",
     databaseURL: "https://juntos-trinidad.firebaseio.com",
     projectId: "juntos-trinidad",
     storageBucket: "juntos-trinidad.appspot.com",
-    messagingSenderId: "706370720365"
+    messagingSenderId: "XXXXXXXXXXXXXXXXXXX"
   }
 };
